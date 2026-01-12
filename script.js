6a04859c20e653a70894fc252262c511e859ae26
murshid0078 <murshid00086@gmail.com>
2026-01-12 23:26:03 +0530

    Updated website

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function typeEffect() {
 typeEffect();
 
 // Slideshow
-let photos = ["photos/1.jpg", "photos/2.jpg", "photos/3.jpg"];
+let photos = ["photos/1.jpg", "photos/2.jpg", "photos/3.jpg", "photos/4.jpg", "photos/5.jpg", "photos/6.jpg", "photos/7.jpg", "photos/8.jpg", "photos/9.jpg", "photos/10.jpg", "photos/12.jpg", "photos/13.jpg", "photos/14.jpg"];
 let i = 0;
 setInterval(() => {
   i = (i + 1) % photos.length;
